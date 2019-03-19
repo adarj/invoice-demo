546e1100f41ab95078042f8a0fad50d16e44b91d
Akil Darjean <akildarjean@protonmail.com>
2019-03-19 18:15:23 -0500

    Switch to single line comments

--- resources/config.php
+++ resources/config.php
@@ -1,23 +1,17 @@
 <?php
 
-/*
- * Constants for often-used paths
- */
+# Constants for often-used paths
 defined("LIBRARY_PATH")
     or define("LIBRARY_PATH", realpath(dirname(__FILE__) . "/library"));
 
 defined("TEMPLATES_PATH")
     or define("TEMPLATES_PATH", realpath(dirname(__FILE__) . "/templates"));
 
-/*
- * Error reporting
- */
+# Set error reporting on
 ini_set("error_reporting", "true");
 error_reporting(E_ALL|E_STRICT);
 
-/*
- * Initialize database
- */
+# Initialize database
 class InvoiceAppDB extends SQLite3 {
     function __construct() {
         $this->open("invoice_app.db");
