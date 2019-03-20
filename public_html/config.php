2201f3aa1def094bffc233e08e393b849c3922e9
Akil Darjean <akildarjean@protonmail.com>
2019-03-19 20:41:18 -0500

    Remove useless function

--- public_html/config.php
+++ public_html/config.php
@@ -9,19 +9,6 @@ class InvoiceAppDB extends SQLite3 {
     function __construct() {
         $this->open("invoice_app.db");
     }
-
-    function init_tables() {
-        $sql = <<<EOD
-CREATE TABLE users (
-    id INT NOT NULL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-);
-EOD;
-
-        $this->exec($sql);
-    }
 }
 
 $database_exists = False;
@@ -32,7 +19,16 @@ if (file_exists("invoice_app.db")) {
 $db = new InvoiceAppDB();
 
 if (!$database_exists) {
-    $db->init_tables();
+    $sql = <<<EOD
+CREATE TABLE users (
+    id INT NOT NULL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+EOD;
+
+    $db->exec($sql);
 }
 
 ?>
