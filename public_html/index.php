511b5370309eece00880be7a678e0e510856d6e5
Akil Darjean <akildarjean@protonmail.com>
2019-03-25 15:52:55 -0500

    Direct user to home page if logged in

--- public_html/index.php
+++ public_html/index.php
@@ -1,6 +1,7 @@
 <?php
 # If the user is not logged in, then open login page
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    require_once("home.php");
 } else {
     require_once("login.php");
 }
