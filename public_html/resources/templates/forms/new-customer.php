848ad68936a6dd76d789bbe6e27dc5658da56bac
Akil Darjean <akildarjean@protonmail.com>
2019-03-25 21:47:03 -0500

    Create form to input new customer data

---
+++ public_html/resources/templates/forms/new-customer.php
@@ -0,0 +1,20 @@
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-6" id="newCustomerForm">
+          <h1 class="text-center mt-3">New Customer</h1>
+          <div class="container">
+            <form method="post">
+              <div class="form-group">
+                <label for="firstNameInput">First name</label>
+                <input type="text" class="form-control" name="first-name" id="firstNameInput" required>
+              </div>
+              <div class="form-group">
+                <label for="lastNameInput">Last name</label>
+                <input type="text" class="form-control" name="last-name" id="lastNameInput" required>
+              </div>
+              <button type="submit" class="btn btn-primary">Add Customer</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
