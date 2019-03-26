92a25fec26723e5457a3150ed3b089d8f9d3538b
Akil Darjean <akildarjean@protonmail.com>
2019-03-25 19:03:28 -0500

    Use same standard for all id tags

--- public_html/resources/templates/forms/register.php
+++ public_html/resources/templates/forms/register.php
@@ -1,21 +1,21 @@
 <body>
   <div class="container">
     <div class="row justify-content-center vertical-center">
-      <div class="col-4" id="login-form">
+      <div class="col-4" id="registerForm">
         <div class="container">
           <h2>Register</h2>
           <form method="post">
             <div class="form-group">
-              <label for="username-input">Username</label>
-              <input type="text" class="form-control" name="username" id="username-input" required>
+              <label for="usernameInput">Username</label>
+              <input type="text" class="form-control" name="username" id="usernameInput" required>
             </div>
             <div class="form-group">
-              <label for="password-input">Password</label>
-              <input type="password" class="form-control" name="password" id="password-input" required>
+              <label for="passwordInput">Password</label>
+              <input type="password" class="form-control" name="password" id="passwordInput" required>
             </div>
             <div class="form-group">
-              <label for="password-confirm-input">Confirm Password</label>
-              <input type="password" class="form-control" id="password-confirm-input" required>
+              <label for="confirmPasswordInput">Confirm Password</label>
+              <input type="password" class="form-control" id="confirmPasswordInput" required>
             </div>
             <button type="submit" class="btn btn-primary">Create Account</button>
           </form>
