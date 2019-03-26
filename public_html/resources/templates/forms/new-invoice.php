8a2b40eb8dfe803927247eb2fc83c1f196fa33d0
Akil Darjean <akildarjean@protonmail.com>
2019-03-25 22:49:12 -0500

    Add upload file option

---
+++ public_html/resources/templates/forms/new-invoice.php
@@ -0,0 +1,38 @@
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-6" id="newCustomerForm">
+          <h1 class="text-center mt-3">New Invoice</h1>
+          <div class="container">
+            <form method="post" enctype="multipart/form-data">
+              <div class="form-row">
+                <div class="form-group col-md-6">
+                  <label for="invoiceNumberInput">Invoice Number</label>
+                  <input type="text" class="form-control" name="invoice-number" id="invoiceNumberInput" required>
+                </div>
+                <div class="form-group col-md-6">
+                  <label for="invoiceDateInput">Invoice Date</label>
+                  <input type="date" class="form-control" name="invoice-date" id="invoiceDateInput" required>
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="invoiceAmountInput">Amount</label>
+                <input type="text" class="form-control" name="invoice-amount" id="invoiceAmountInput" required>
+              </div>
+              <div class="form-group">
+                <label for="invoiceStatusInput">Status</label>
+                <select class="form-control" name="invoice-status" id="invoiceStatusInput" required>
+                  <option>Billed</option>
+                  <option>Paid</option>
+                </select>
+              </div>
+              <div class="form-group">
+                <label for="invoiceUploadFile">Upload File</label>
+                <br>
+                <input type="file" name="invoice-file" id="invoiceUploadFile">
+              </div>
+              <button type="submit" class="btn btn-primary">Add Invoice</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
