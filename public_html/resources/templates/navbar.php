ff2af286d55f011ffcf829021ab9f11c69273830
Akil Darjean <akildarjean@protonmail.com>
2019-03-25 20:09:15 -0500

    Add 'Add Customer' button to navbar

--- public_html/resources/templates/navbar.php
+++ public_html/resources/templates/navbar.php
@@ -1,16 +1,24 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <a class="navbar-brand" href="/">Invoice Demo</a>
-    
+
       <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-    
+
       <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav mr-auto">
         </ul>
         <ul class="navbar-nav">
-          <li class="nav-item dropdown" id="accountDropdownButton">
-            <button type="button" class="btn btn-primary dropdown-toggle pl-3 pr-2" id="accountDropdownButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <li class="nav-item dropdown" id="addDropdown">
+            <button type="button" class="btn btn-dark dropdown-toggle pl-3 pr-2" id="addDropdownButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <i class="fas fa-plus"></i>
+            </button>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+              <a class="dropdown-item" href="/add-customer/">Add Customer</a>
+            </div>
+          </li>
+          <li class="nav-item dropdown" id="accountDropdown">
+            <button type="button" class="btn btn-primary dropdown-toggle pl-3 pr-2 ml-2 mr-2" id="accountDropdownButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               <i class="fas fa-user"></i>
             </button>
             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
