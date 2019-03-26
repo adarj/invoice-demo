f5fcfc5584ab1762790c6c226f79393e43cab20a
Akil Darjean <akildarjean@protonmail.com>
2019-03-25 19:08:38 -0500

    Move <body> tags to header and footer templates

--- public_html/resources/templates/forms/login.php
+++ public_html/resources/templates/forms/login.php
@@ -1,23 +1,21 @@
-<body>
-  <div class="container">
-    <div class="row justify-content-center vertical-center">
-      <div class="col-4" id="loginForm">
-        <h1 class="text-center">Invoice Demo</h1>
-        <div class="container">
-          <form method="post">
-            <div class="form-group">
-              <label for="usernameInput">Username</label>
-              <input type="text" class="form-control" name="username" id="usernameInput" required>
-            </div>
-            <div class="form-group">
-              <label for="passwordInput">Password</label>
-              <input type="password" class="form-control" name="password" id="passwordInput" required>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-          </form>
+    <div class="container">
+      <div class="row justify-content-center vertical-center">
+        <div class="col-4" id="loginForm">
+          <h1 class="text-center">Invoice Demo</h1>
+          <div class="container">
+            <form method="post">
+              <div class="form-group">
+                <label for="usernameInput">Username</label>
+                <input type="text" class="form-control" name="username" id="usernameInput" required>
+              </div>
+              <div class="form-group">
+                <label for="passwordInput">Password</label>
+                <input type="password" class="form-control" name="password" id="passwordInput" required>
+              </div>
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+          </div>
+          <p class="text-center">Need an account? Register <a href="register.php">here</a></p>
         </div>
-        <p class="text-center">Need an account? Register <a href="register.php">here</a></p>
       </div>
     </div>
-  </div>
-</body>
--- public_html/resources/templates/forms/register.php
+++ public_html/resources/templates/forms/register.php
@@ -1,27 +1,25 @@
-<body>
-  <div class="container">
-    <div class="row justify-content-center vertical-center">
-      <div class="col-4" id="registerForm">
-        <div class="container">
-          <h2>Register</h2>
-          <form method="post">
-            <div class="form-group">
-              <label for="usernameInput">Username</label>
-              <input type="text" class="form-control" name="username" id="usernameInput" required>
-            </div>
-            <div class="form-group">
-              <label for="passwordInput">Password</label>
-              <input type="password" class="form-control" name="password" id="passwordInput" required>
-            </div>
-            <div class="form-group">
-              <label for="confirmPasswordInput">Confirm Password</label>
-              <input type="password" class="form-control" id="confirmPasswordInput" required>
-            </div>
-            <button type="submit" class="btn btn-primary">Create Account</button>
-          </form>
+    <div class="container">
+      <div class="row justify-content-center vertical-center">
+        <div class="col-4" id="registerForm">
+          <div class="container">
+            <h2>Register</h2>
+            <form method="post">
+              <div class="form-group">
+                <label for="usernameInput">Username</label>
+                <input type="text" class="form-control" name="username" id="usernameInput" required>
+              </div>
+              <div class="form-group">
+                <label for="passwordInput">Password</label>
+                <input type="password" class="form-control" name="password" id="passwordInput" required>
+              </div>
+              <div class="form-group">
+                <label for="confirmPasswordInput">Confirm Password</label>
+                <input type="password" class="form-control" id="confirmPasswordInput" required>
+              </div>
+              <button type="submit" class="btn btn-primary">Create Account</button>
+            </form>
+          </div>
+          <p class="text-center">Already have an account? Log in <a href="/">here</a></p>
         </div>
-        <p class="text-center">Already have an account? Log in <a href="/">here</a></p>
       </div>
     </div>
-  </div>
-</body>
