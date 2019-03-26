50047f3e58878dffc5e43a87ba1d91bc8748ba63
Akil Darjean <akildarjean@protonmail.com>
2019-03-25 20:39:32 -0500

    Add 'Add Customer' page

---
+++ public_html/add-customer/index.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+
+require_once("../config.php");
+
+# If the user is not logged in, then open login page
+if(!isset($_SESSION["loggedIn"]) || $_SESSION["loggedIn"] !== True){
+    header("location: /");
+    exit();
+}
+
+# Load HTML/CSS
+require_once(root_path . "/resources/templates/header.php");
+require_once(root_path . "/resources/templates/navbar.php");
+require_once(root_path . "/resources/templates/footer.php");
+?>
