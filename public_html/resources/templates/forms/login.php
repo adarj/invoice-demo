3da89ed7462b0d3ae7cfb58b761bbd5194fea837
Akil Darjean <akildarjean@protonmail.com>
2019-03-27 10:49:24 -0500

    Add client side validation

--- public_html/resources/templates/forms/login.php
+++ public_html/resources/templates/forms/login.php
@@ -3,14 +3,20 @@
         <div class="col-4" id="loginForm">
           <h1 class="text-center">Invoice Demo</h1>
           <div class="container">
-            <form method="post">
+            <form method="post" class="needs-validation" novalidate>
               <div class="form-group">
                 <label for="usernameInput">Username</label>
                 <input type="text" class="form-control" name="username" id="usernameInput" required>
+                <div class="invalid-feedback">
+                  Please enter a username.
+                </div>
               </div>
               <div class="form-group">
                 <label for="passwordInput">Password</label>
                 <input type="password" class="form-control" name="password" id="passwordInput" required>
+                <div class="invalid-feedback">
+                  Please enter a password.
+                </div>
               </div>
               <button type="submit" class="btn btn-primary">Submit</button>
             </form>
@@ -20,3 +26,21 @@
         </div>
       </div>
     </div>
+
+<script>
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+        var forms = document.getElementsByClassName('needs-validation');
+        var validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+            if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
+</script>
