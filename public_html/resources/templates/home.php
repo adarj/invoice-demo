d0324aa3c429ea48b43e9b47a3ea32db0c0d7eae
Akil Darjean <akildarjean@protonmail.com>
2019-03-27 01:15:13 -0500

    Create home page template

---
+++ public_html/resources/templates/home.php
@@ -0,0 +1,10 @@
+    <div class="container">
+      <div class="row justify-content-center vertical-center">
+        <div class="col" id="welcome">
+          <div class="container">
+            <h1 class="text-center">Welcome!</h1>
+            <h3 class="text-center">You have <?php echo count(getInvoices()); ?> invoices.</h3>
+          </div>
+        </div>
+      </div>
+    </div>
